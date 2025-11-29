fac81bc7fb5d066add0d1d1ec67e1c444ee42c02
Thu <xuanthule0712@gmail.com>
2025-11-29 09:39:43 +0700

    test

--- Nhom09_QuanLyKaraoke/DichVu.Designer.cs
+++ Nhom09_QuanLyKaraoke/DichVu.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label3
@@ -42,11 +43,21 @@
             this.label3.TabIndex = 37;
             this.label3.Text = "DỊCH VỤ";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 170);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "label1";
+            // 
             // DichVu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1082, 473);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
             this.Name = "DichVu";
             this.Text = "DichVu";
@@ -58,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
     }
 }
